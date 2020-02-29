3935beea13600c152398c2996106a06ed6aafa6d
Gabriel Oliveira <gabriel.oliveira@engdb.com.br>
2020-02-29 17:21:08 -0300

    Configurações iniciais

---
+++ src/main/java/br/com/brazilcode/cb/administration/CbAdministrationServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.brazilcode.cb.administration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbAdministrationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbAdministrationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/brazilcode/cb/administration/cbadministrationservice/CbAdministrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brazilcode.cb.administration.cbadministrationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbAdministrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
